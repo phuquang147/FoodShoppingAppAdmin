40b4bebd43cc981db445f0054f7266ad5bfe02e2
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-05-26 09:35:33 +0700

    Report ui

--- app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/ui/report/ReportFragment.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/ui/report/ReportFragment.kt
@@ -1,9 +1,11 @@
 package xyz.daijoubuteam.foodshoppingappadmin.ui.report
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -25,8 +27,29 @@ class ReportFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_report, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
-
+        binding.viewmodel = viewModel
+        setupMonthAndYearDropDownMenu()
         return binding.root
     }
 
+    private fun setupMonthAndYearDropDownMenu() {
+        val months = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+        val monthAdapter = ArrayAdapter(this.requireContext(), R.layout.item_dropdown, months)
+        val years = listOf("2021", "2022")
+        val yearAdapter = ArrayAdapter(this.requireContext(), R.layout.item_dropdown, years)
+        binding.monthAuto.setAdapter(monthAdapter)
+        binding.monthAuto.threshold = 100
+        binding.yearAuto.setAdapter(yearAdapter)
+        binding.yearAuto.threshold = 100
+
+        binding.monthAuto.setOnItemClickListener { _, _, position, _ ->
+            val value = monthAdapter.getItem(position) ?: ""
+            Log.i("buttonclick", value)
+        }
+
+        binding.yearAuto.setOnItemClickListener { _, _, position, _ ->
+            val value = yearAdapter.getItem(position) ?: ""
+            Log.i("buttonclick", value)
+        }
+    }
 }
--- app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/ui/report/ReportViewModel.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/ui/report/ReportViewModel.kt
@@ -1,7 +1,26 @@
 package xyz.daijoubuteam.foodshoppingappadmin.ui.report
 
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import xyz.daijoubuteam.foodshoppingappadmin.MainApplication
+import xyz.daijoubuteam.foodshoppingappadmin.model.Eatery
 
 class ReportViewModel : ViewModel() {
+    private var _eatery = MutableLiveData<Eatery>(null)
+    val eatery: LiveData<Eatery>
+        get() = _eatery
 
+    val month =  MutableLiveData("1")
+    val year = MutableLiveData("2021")
+
+    init {
+        viewModelScope.launch {
+            _eatery = MainApplication.eatery
+        }
+    }
 }
