fdabc94369394e6d104ee77384f92c6a4f662e33
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-06-05 15:33:14 +0700

    Update login logout

--- app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/SplashScreen.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/SplashScreen.kt
@@ -1,16 +1,33 @@
 package xyz.daijoubuteam.foodshoppingappadmin
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.launch
 import xyz.daijoubuteam.foodshoppingappadmin.authentication.AuthActivity
+import xyz.daijoubuteam.foodshoppingappadmin.repositories.EateryRepository
+import xyz.daijoubuteam.foodshoppingappadmin.utils.Constants
+import androidx.lifecycle.lifecycleScope
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+    private val eateryRepository = EateryRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        val sharedPref =
+            this.getSharedPreferences(Constants.USER_PREF, Context.MODE_PRIVATE) ?: return
+        val username = sharedPref.getString(Constants.USERNAME, null)
+
+        if (username != null) {
+            lifecycleScope.launch {
+                val eatery = eateryRepository.getEateryByUsername(username)
+                MainApplication.eatery.value = eatery.getOrNull()
+            }
+        }
 
         Handler().postDelayed({
             MainApplication.eatery.observe(this) {
@@ -18,13 +35,12 @@ class SplashScreen : AppCompatActivity() {
                     val intent = Intent(this, AuthActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(intent)
-                }
-                else {
+                } else {
                     val intent = Intent(this, MainActivity::class.java)
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(intent)
                 }
             }
-        }, 1000)
+        }, 2000)
     }
 }
--- app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/ui/profile/ProfileFragment.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/ui/profile/ProfileFragment.kt
@@ -1,22 +1,22 @@
 package xyz.daijoubuteam.foodshoppingappadmin.ui.profile
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import xyz.daijoubuteam.foodshoppingappadmin.MainApplication
 import xyz.daijoubuteam.foodshoppingappadmin.R
-import xyz.daijoubuteam.foodshoppingappadmin.databinding.FragmentEditProductBinding
+import xyz.daijoubuteam.foodshoppingappadmin.authentication.AuthActivity
 import xyz.daijoubuteam.foodshoppingappadmin.databinding.FragmentProfileBinding
-import xyz.daijoubuteam.foodshoppingappadmin.model.Product
-import xyz.daijoubuteam.foodshoppingappadmin.ui.products.ProductsFragmentDirections
-import xyz.daijoubuteam.foodshoppingappadmin.ui.products.ProductsViewModelFactory
-import xyz.daijoubuteam.foodshoppingappadmin.ui.products.editproduct.EditProductFragmentArgs
-import xyz.daijoubuteam.foodshoppingappadmin.ui.products.editproduct.EditProductViewModel
-import xyz.daijoubuteam.foodshoppingappadmin.ui.products.editproduct.EditProductViewModelFactory
+import xyz.daijoubuteam.foodshoppingappadmin.utils.Constants
+import xyz.daijoubuteam.foodshoppingappadmin.utils.Constants.USERNAME
+
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
@@ -37,7 +37,7 @@ class ProfileFragment : Fragment() {
         binding.viewModel = viewModel
         setupNavigateToReviewsFragment()
         setupNavigateToEditProfileFragment()
-
+        setupNavigateToLoginFragment()
         return binding.root
     }
 
@@ -50,6 +50,21 @@ class ProfileFragment : Fragment() {
     private fun setupNavigateToEditProfileFragment() {
         binding.btnProfileAndAddress.setOnClickListener {
             findNavController().navigate(ProfileFragmentDirections.actionNavigationProfileToEditProfileFragment())
+        }
+    }
+
+    private fun setupNavigateToLoginFragment() {
+        val sharedPref = this.requireActivity()
+            .getSharedPreferences(Constants.USER_PREF, Context.MODE_PRIVATE)
+        binding.btnLogout.setOnClickListener {
+            with(sharedPref?.edit()) {
+                this?.remove(USERNAME)
+                this?.apply()
+            }
+            MainApplication.eatery.value = null
+            val intent = Intent(activity, AuthActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 }
