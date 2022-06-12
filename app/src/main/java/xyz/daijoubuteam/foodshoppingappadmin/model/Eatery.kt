8e6c9d9ba03a83e3e1787035315668dc4ae8e653
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-06-12 23:47:20 +0700

    Update model

--- app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/model/Eatery.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/model/Eatery.kt
@@ -2,7 +2,10 @@ package xyz.daijoubuteam.foodshoppingappadmin.model
 
 import android.os.Parcelable
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.Exclude
 import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
 @Parcelize
 data class Eatery(
@@ -13,10 +16,12 @@ data class Eatery(
     var work_time: String? = null,
     val average_rating_count: Double? = null,
     var description: String? = null,
-    val reviews: ArrayList<String>? = null,
-    val products: ArrayList<Product>? = null,
     var photoUrl: String? = null,
     val username: String? = null,
     val password: String? = null,
-    var eateryId: String? = null
+    var eateryId: String? = null,
+    @Exclude
+    val products: ArrayList<Product>? = null,
+    @Exclude
+    var eateryPath: @RawValue DocumentReference? = null
 ) : Parcelable
--- app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/model/Event.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingappadmin/model/Event.kt
@@ -2,8 +2,10 @@ package xyz.daijoubuteam.foodshoppingappadmin.model
 
 import android.os.Parcelable
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.Exclude
 import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
 
 @Parcelize
 data class Event (
@@ -12,6 +14,7 @@ data class Event (
     val name: String? = null,
     val content: String? = null,
     val image: String? = null,
+    val idEateryList: @RawValue ArrayList<DocumentReference>? = null,
     @Exclude
     var subscribed: Boolean = false,
 ): Parcelable
